852285a7a3695d0d0f746e45a8551fb55c5c062f
shaik-sabeel <techsabeel1919@gmail.com>
2025-07-14 00:05:12 +0530

    Used constructor and added state

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,17 +3,15 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
 
     render() {
-        let {title, description} = this.props;
+        let {title, description, imageUrl} = this.props;
     return (
       <div>
         <div className="card" style={{ width: "18rem" }}>
-          <img src="https://ichef.bbci.co.uk/ace/branded_sport/1200/cpsprodpb/714c/live/058a1b80-5e7f-11f0-b5c5-012c5796682d.jpg" className="card-img-top" alt="..." />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-           
-            </a>
+            <a href="/" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
